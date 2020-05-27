827ef46bf1292fe7686cc26427863db06f4584f7
jrlvarghese <jrlvarghese@gmail.com>
2020-05-27 19:18:02 +0530

    Add html code to create basic UI.

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-index.php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Messages App</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class='container'>
+            <header>
+                <h1>Messages app</h1>
+            </header>
+            <div class='main'>
+                <form>
+                    <input type='text' name='message' placeholder='Type your message here'>
+                    <input type='text' name='username' placeholder='Input user name'>
+                    <input type='submit' value='Submit'>
+                </form>
+            </div>
+            <hr>
+            <ul class='messages'>
+                <li>Message 1 | Username 1</li>
+                <li>Message 2 | Username 2</li>
+            </ul>
+        </div>
+        <footer>
+            messageapp &copy; 2020
+        </footer>
+    </body>
+</html>
