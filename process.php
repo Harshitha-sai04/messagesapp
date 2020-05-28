37e4462465a7f5c8aa37006319c00f4057e6f3b2
jrlvarghese <jrlvarghese@gmail.com>
2020-05-28 19:55:36 +0530

    Insert messages into database.

--- process.php
+++ process.php
@@ -5,5 +5,17 @@ include 'db.php';
 if($_POST['message']=='' || $_POST['username'=='']){
     header("Location: index.php?error=Please%20Fill%20All%20Fields");
 }else{
-    header("Location: index.php?success=Success");
+    // validate strings that contains valid inputs that will not introduce any bugs
+    $message = mysqli_real_escape_string($connection, $_POST['message']);
+    $username = mysqli_real_escape_string($connection, $_POST['username']);
+    $insertQuery = "INSERT INTO messages(message, user) VALUES('$message', '$username')";
+
+    // check if the connection was successful
+    if(!mysqli_query($connection, $insertQuery)){
+        // if it was not successful
+        die(mysqli_error($connection));
+    }else{
+        // if connection was successful
+        header("Location: index.php?success=Message%20Successfuly%20Inserted.");
+    }
 }
