cc34b8352bb68260c9fe9ba90f730e94e90918f6
jrlvarghese <jrlvarghese@gmail.com>
2020-05-28 18:50:28 +0530

    Validate whether input fields are blank or not using php.

--- process.php
+++ process.php
@@ -1,1 +1,9 @@
-process.php
+<?php
+include 'db.php';
+
+// check whether the fields are empty or not
+if($_POST['message']=='' || $_POST['username'=='']){
+    header("Location: index.php?error=Please%20Fill%20All%Fields");
+}else{
+    echo 'Submitted';
+}
